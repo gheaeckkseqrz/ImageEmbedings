f0cbe6f9fe348060a25940524fc8865c2d67b318
Pierre Wilmot <pierre.wilmot@gmail.com>
2020-04-28 15:29:26 +0100

    Chenge loss function

--- main.cpp
+++ main.cpp
@@ -1,35 +1,68 @@
 #include <torch/torch.h>
 #include <iostream>
+#include <cmath>
 
 #include "feature_extractor.h"
 #include "dataloader.h"
 #include "feature_extractor.h"
 #include "plot.h"
 
-void train(Dataloader &dataloader, std::shared_ptr<FeatureExtractor> model, torch::optim::Adam &optimizer)
+int Z = 2;
+
+void plot(Dataloader &dataloader, std::shared_ptr<FeatureExtractor> model, unsigned int folder_limit)
 {
+  model->eval();
   ScatterPlot p;
-  for (unsigned int i(0) ; i < 100 ; ++i)
+  for (unsigned int folder(0) ; folder < folder_limit ; ++folder)
+  {
+    for (unsigned int i(0) ; i < 10 ; ++i)
+      {
+	torch::Tensor image = dataloader.get(folder, i).unsqueeze(0);
+	torch::Tensor code = model->forward(image);
+	p.addPoint(code[0][0].item<float>(), code[0][1].item<float>(), folder);
+      }
+  }
+  p.display();
+  cv::waitKey(100);
+}
+
+float train(Dataloader &dataloader, std::shared_ptr<FeatureExtractor> model, torch::optim::Adam &optimizer)
+{
+  model->train();
+  float total_loss = 0;
+  torch::Tensor reference_target = torch::zeros(std::vector<int64_t>({Z})).cuda();
+  reference_target[0] = 7;
+  torch::Tensor norm_target = torch::ones(std::vector<int64_t>({1})).cuda();
+  norm_target.fill_(torch::norm(reference_target).item<float>());
+
+  torch::Tensor label_pos = torch::zeros(std::vector<int64_t>({1})).cuda();
+  torch::Tensor label_neg = torch::zeros(std::vector<int64_t>({1})).cuda();
+
+  label_pos.fill_( 1);
+  label_neg.fill_(-1);
+
+  optimizer.zero_grad();
+  for (unsigned int i(0) ; i < 32 ; ++i)
     {
       Triplet training_triplet = dataloader.getTriplet();
-      torch::Tensor anchor_code = model->forward(training_triplet.anchor.unsqueeze(0)).detach();
-      torch::Tensor same_code = model->forward(training_triplet.same.unsqueeze(0));
-      torch::Tensor diff_code = model->forward(training_triplet.diff.unsqueeze(0));
+      torch::Tensor anchor_code = model->forward(training_triplet.anchor.unsqueeze(0)).detach() * 10;
+      torch::Tensor same_code = model->forward(training_triplet.same.unsqueeze(0)) * 10;
+      torch::Tensor diff_code = model->forward(training_triplet.diff.unsqueeze(0)) * 10;
 
-      std::cout << anchor_code.sizes() << std::endl;
-      p.addPoint(anchor_code[0][0].item<float>(), anchor_code[0][1].item<float>(), training_triplet.anchor_folder_index);
-      p.addPoint(same_code[0][0].item<float>(), anchor_code[0][1].item<float>(), training_triplet.anchor_folder_index);
-      p.addPoint(diff_code[0][0].item<float>(), anchor_code[0][1].item<float>(), training_triplet.diff_folder_index);
+      torch::Tensor norm_same = torch::norm(same_code);
+      torch::Tensor norm_diff = torch::norm(diff_code);
 
-      optimizer.zero_grad();
-      torch::Tensor loss_same = torch::mse_loss(same_code, anchor_code);
-      torch::Tensor loss_diff = torch::mse_loss(diff_code, anchor_code);
-      torch::Tensor loss = loss_same - loss_diff;
+      torch::Tensor loss_same = torch::cosine_embedding_loss(same_code, anchor_code, label_pos, .2);
+      torch::Tensor loss_diff = torch::cosine_embedding_loss(diff_code, anchor_code, label_neg, .2);
+      torch::Tensor loss_norm_same = torch::mse_loss(norm_same, norm_target) / 500;
+      torch::Tensor loss_norm_diff = torch::mse_loss(norm_diff, norm_target) / 500;
+
+      torch::Tensor loss = loss_same + loss_diff + loss_norm_same + loss_norm_diff;
+      total_loss += loss.item<float>();
       loss.backward();
-      optimizer.step();
-      p.display();
-      cv::waitKey(100);
     }
+  optimizer.step();
+  return total_loss;
 }
 
 int main(int ac, char **av)
@@ -41,10 +74,19 @@ int main(int ac, char **av)
     }
 
   Dataloader dataloader(av[1], 256);
-  auto model = std::make_shared<FeatureExtractor>(1, 2);
-  torch::optim::Adam optimizer(model->parameters(), 0.01);
+  dataloader.fillCache(20, 10);
+  auto model = std::make_shared<FeatureExtractor>(32, 2);
+  model->to(at::kCUDA);
+  torch::optim::Adam optimizer(model->parameters(), 0.0001);
 
-  train(dataloader, model, optimizer);
+  unsigned int folder_limit = 20;
+  dataloader.setLimits(10, folder_limit);
+  while (true)
+    {
+      for (int i(0) ; i  < 50 ; ++i)
+      	std::cout << folder_limit << " -- " << i << " -- " << train(dataloader, model, optimizer) << std::endl;
+       plot(dataloader, model, folder_limit);
+    }
 
   cv::waitKey(0);
   return 0;
