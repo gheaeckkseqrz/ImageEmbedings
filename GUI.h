3cdb7b0dbcf3cc793d67c62d955a91f899e33432
Pierre Wilmot <pierre.wilmot@gmail.com>
2020-12-14 08:48:44 +0000

    Make GUI preview radius dynamic

--- GUI.h
+++ GUI.h
@@ -17,7 +17,7 @@ struct Item
 
 class GUIDelegate
 {
- public:
+public:
   virtual void increaseFileLimit() = 0;
   virtual void decreaseFileLimit() = 0;
   virtual void increaseFolderLimit() = 0;
@@ -32,9 +32,9 @@ class GUIDelegate
 
 class GUI
 {
- public:
+public:
   GUI(GUIDelegate * delegate = nullptr)
-    :_delegate(delegate), _max(1)
+    :_delegate(delegate), _max(1), _radius(5)
   {
     _colors.push_back({0,     0, 255});
     _colors.push_back({0,   255,   0});
@@ -75,10 +75,10 @@ class GUI
   {
     std::scoped_lock lock(_pointActiveM);
     for (Item &i : _pointsNext)
-      {
-	i.x /= _max;
-	i.y /= _max;
-      }
+    {
+      i.x /= _max;
+      i.y /= _max;
+    }
     _pointsActive.swap(_pointsNext);
     _pointsNext.clear();
     _max = 1;
@@ -91,120 +91,130 @@ class GUI
     _diff = d;
   }
 
- private:
+private:
   void _start()
   {
     _window.create(sf::VideoMode(2000, 2000), "My window");
     _alive = true;
     while (_alive)
+    {
+      sf::Event event;
+      while (_window.pollEvent(event))
       {
-	sf::Event event;
-        while (_window.pollEvent(event))
-	  {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-	      {
-		_window.close();
-		break;
-	      }
-	    if (event.type == sf::Event::KeyReleased && _delegate)
-	      {
-		switch(event.key.code)
-		  {
-		  case sf::Keyboard::Q:
-		    _delegate->increaseFolderLimit();
-		      break;
-		  case sf::Keyboard::A:
-		    _delegate->decreaseFolderLimit();
-		      break;
-		  case sf::Keyboard::W:
-		    _delegate->increaseFileLimit();
-		      break;
-		  case sf::Keyboard::S:
-		    _delegate->decreaseFileLimit();
-		      break;
-		  case sf::Keyboard::E:
-		    _delegate->increaseMargin();
-		      break;
-		  case sf::Keyboard::D:
-		    _delegate->decreaseMargin();
-		      break;
-		  case sf::Keyboard::R:
-		    _delegate->increaseSampling();
-		    break;
-		  case sf::Keyboard::F:
-		    _delegate->decreaseSampling();
-		    break;
-		  case sf::Keyboard::T:
-		    _delegate->increaseDisplayEvery();
-		    break;
-		  case sf::Keyboard::G:
-		    _delegate->decreaseDisplayEvery();
-		    break;
-		  default:
-		    break;
-		  }
-	      }
-	  }
-	sf::Vector2i mouse = sf::Mouse::getPosition(_window);
-	_window.clear(sf::Color(10, 10, 10));
+	// "close requested" event: we close the window
+	if (event.type == sf::Event::Closed)
 	{
-	  std::scoped_lock lock(_pointActiveM);
-	  unsigned int x(0);
-	  for (Item item : _pointsActive)
-	    {
-	      item.x *= 1000;
-	      item.y *= 1000;
-	      item.x += 1000;
-	      item.y += 1000;
-	      sf::CircleShape shape(5.f);
-	      shape.setFillColor(_colors[item.label]);
-	      shape.setPosition(item.x - 5, item.y / _max - 5);
-	      if (std::pow(item.x - mouse.x, 2) + std::pow(item.y - mouse.y, 2) < 25)
-		{
-		  shape.setOutlineThickness(2.f);
-		  shape.setOutlineColor(sf::Color(255, 0, 0));
-		  sf::Sprite sprite;
-		  sf::Texture texture;
-		  texture.loadFromFile(item.path);
-		  sf::Vector2u size = texture.getSize();
-		  sprite.setTexture(texture);
-		  sprite.setScale(256.0 / size.x, 256.0 / size.y);
-		  sprite.setPosition(x, 0);
-		  _window.draw(sprite);
-		  x += 256;
-		}
-	      _window.draw(shape);
-	    }
+	  _window.close();
+	  break;
 	}
-	if (_anchor.size() && _same.size() && _diff.size())
+	if (event.type == sf::Event::MouseWheelMoved)
+	{
+	  _radius += event.mouseWheel.delta;
+	}
+	if (event.type == sf::Event::KeyReleased && _delegate)
+	{
+	  switch(event.key.code)
 	  {
-	    sf::Sprite sprite1, sprite2, sprite3;
-	    sf::Texture texture1, texture2, texture3;
-	    texture1.loadFromFile(_anchor);
-	    texture2.loadFromFile(_same);
-	    texture3.loadFromFile(_diff);
-	    sf::Vector2u size1 = texture1.getSize();
-	    sf::Vector2u size2 = texture2.getSize();
-	    sf::Vector2u size3 = texture3.getSize();
-	    sprite1.setTexture(texture1);
-	    sprite2.setTexture(texture2);
-	    sprite3.setTexture(texture3);
-	    sprite1.setScale(256.0 / size1.x, 256.0 / size1.y);
-	    sprite2.setScale(256.0 / size2.x, 256.0 / size2.y);
-	    sprite3.setScale(256.0 / size3.x, 256.0 / size3.y);
-	    sprite1.setPosition(  0, 2000 - 256);
-	    sprite2.setPosition(256, 2000 - 256);
-	    sprite3.setPosition(512, 2000 - 256);
-	    _window.draw(sprite1);
-	    _window.draw(sprite2);
-	    _window.draw(sprite3);
+	  case sf::Keyboard::Q:
+	    _delegate->increaseFolderLimit();
+	    break;
+	  case sf::Keyboard::A:
+	    _delegate->decreaseFolderLimit();
+	    break;
+	  case sf::Keyboard::W:
+	    _delegate->increaseFileLimit();
+	    break;
+	  case sf::Keyboard::S:
+	    _delegate->decreaseFileLimit();
+	    break;
+	  case sf::Keyboard::E:
+	    _delegate->increaseMargin();
+	    break;
+	  case sf::Keyboard::D:
+	    _delegate->decreaseMargin();
+	    break;
+	  case sf::Keyboard::R:
+	    _delegate->increaseSampling();
+	    break;
+	  case sf::Keyboard::F:
+	    _delegate->decreaseSampling();
+	    break;
+	  case sf::Keyboard::T:
+	    _delegate->increaseDisplayEvery();
+	    break;
+	  case sf::Keyboard::G:
+	    _delegate->decreaseDisplayEvery();
+	    break;
+	  default:
+	    break;
 	  }
-	_window.display();
+	}
       }
+      sf::Vector2i mouse = sf::Mouse::getPosition(_window);
+      _window.clear(sf::Color(10, 10, 10));
+      {
+	std::scoped_lock lock(_pointActiveM);
+	unsigned int x(0);
+	sf::CircleShape mshape(_radius);
+	mshape.setPosition(mouse.x - _radius, mouse.y - _radius);
+	mshape.setOutlineThickness(2.f);
+	mshape.setOutlineColor(sf::Color::White);
+	mshape.setFillColor(sf::Color::Black);
+	_window.draw(mshape);
+	for (Item item : _pointsActive)
+	{
+	  item.x *= 1000;
+	  item.y *= 1000;
+	  item.x += 1000;
+	  item.y += 1000;
+	  sf::CircleShape shape(5.f);
+	  shape.setFillColor(_colors[item.label]);
+	  shape.setPosition(item.x - 5, item.y / _max - 5);
+	  if (std::pow(item.x - mouse.x, 2) + std::pow(item.y - mouse.y, 2) < _radius * _radius)
+	  {
+	    shape.setOutlineThickness(2.f);
+	    shape.setOutlineColor(sf::Color(255, 0, 0));
+	    sf::Sprite sprite;
+	    sf::Texture texture;
+	    texture.loadFromFile(item.path);
+	    sf::Vector2u size = texture.getSize();
+	    sprite.setTexture(texture);
+	    sprite.setScale(256.0 / size.x, 256.0 / size.y);
+	    sprite.setPosition(x, 0);
+	    _window.draw(sprite);
+	    x += 256;
+	  }
+	  _window.draw(shape);
+	}
+      }
+      if (_anchor.size() && _same.size() && _diff.size())
+      {
+	sf::Sprite sprite1, sprite2, sprite3;
+	sf::Texture texture1, texture2, texture3;
+	texture1.loadFromFile(_anchor);
+	texture2.loadFromFile(_same);
+	texture3.loadFromFile(_diff);
+	sf::Vector2u size1 = texture1.getSize();
+	sf::Vector2u size2 = texture2.getSize();
+	sf::Vector2u size3 = texture3.getSize();
+	sprite1.setTexture(texture1);
+	sprite2.setTexture(texture2);
+	sprite3.setTexture(texture3);
+	sprite1.setScale(256.0 / size1.x, 256.0 / size1.y);
+	sprite2.setScale(256.0 / size2.x, 256.0 / size2.y);
+	sprite3.setScale(256.0 / size3.x, 256.0 / size3.y);
+	sprite1.setPosition(  0, 2000 - 256);
+	sprite2.setPosition(256, 2000 - 256);
+	sprite3.setPosition(512, 2000 - 256);
+	_window.draw(sprite1);
+	_window.draw(sprite2);
+	_window.draw(sprite3);
+      }
+      _window.display();
+    }
   }
 
- private:
+private:
   std::vector<Item>      _pointsNext;
   std::vector<Item>      _pointsActive;
   std::mutex             _pointActiveM;
@@ -214,6 +224,7 @@ class GUI
   std::vector<sf::Color> _colors;
   GUIDelegate           *_delegate;
   double                 _max;
+  int                    _radius;
 
   // Triplet Display
   std::string _anchor;
